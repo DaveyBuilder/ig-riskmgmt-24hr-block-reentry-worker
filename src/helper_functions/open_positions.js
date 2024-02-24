3f01c3bc108d7b897064ade691af8b7b5acca036
david586 <d.harding1020@gmail.com>
2024-02-24 20:56:32 +0700

    API retries

--- src/helper_functions/open_positions.js
+++ src/helper_functions/open_positions.js
@@ -1,35 +1,32 @@
 export async function getOpenPositions(env, CST, X_SECURITY_TOKEN, baseURL) {
 
-    const openPositionsResponse = await fetch(`${baseURL}/positions`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-IG-API-KEY': env.IG_API_KEY,
-            'Version': '2',
-            'CST': CST,
-            'X-SECURITY-TOKEN': X_SECURITY_TOKEN
-        }
-    });
+    let attempts = 1;
+    let openPositionsResponse;
+    while (attempts <= 3) {
 
-    if (!openPositionsResponse.ok) {
-        throw new Error(`Error getting open positions. HTTP status: ${openPositionsResponse.status}`);
+        openPositionsResponse = await fetch(`${baseURL}/positions`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-IG-API-KEY': env.IG_API_KEY,
+                'Version': '2',
+                'CST': CST,
+                'X-SECURITY-TOKEN': X_SECURITY_TOKEN
+            }
+        });
+
+        if (openPositionsResponse.ok) {
+            console.log(`Get open positions API attempt ${attempts} succeeded`);
+            const openPositionsData = await openPositionsResponse.json();
+            return openPositionsData;
+        } else {
+            const responseBody = await openPositionsResponse.json();
+            console.log(`Attempt ${attempts} failed with status: ${openPositionsResponse.status}, Response: ${JSON.stringify(responseBody, null, 2)}`);
+            attempts++;
+            if (attempts > 3) {
+                throw new Error(`Error getting open positions. HTTP status: ${openPositionsResponse.status}, Response: ${JSON.stringify(responseBody, null, 2)}`);
+            }
+        }
     }
 
-    const openPositionsData = await openPositionsResponse.json();
-
-    let openPositions = {};
-
-    openPositionsData.positions.forEach(position => {
-
-        const instrumentName = position.market.instrumentName;
-
-        if (openPositions[instrumentName]) {
-            openPositions[instrumentName].positions.push(position);
-        } else {
-            openPositions[instrumentName] = { positions: [position] };
-        }
-
-    });
-
-    return openPositions;
 }
