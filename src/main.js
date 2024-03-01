fbb54ebbf8c0f232b4ea911e2234bcf368a62bd9
david586 <d.harding1020@gmail.com>
2024-03-01 07:09:24 +0700

    Remove exception

--- src/main.js
+++ src/main.js
@@ -70,7 +70,7 @@ export async function executeScheduledTask(request, env, ctx, usingDemoAccount) 
     // Create the array that contains the details needed for closure
     const positionsToClose = [];
     for (const item of positionsWithin24Hours) {
-        if (item.market.marketStatus === "TRADEABLE" && item.market.instrumentName !== "Japan 225") {
+        if (item.market.marketStatus === "TRADEABLE") {
             const positionDetailsForClosure = {
                 dealId: item.position.dealId,
                 epic: null,
